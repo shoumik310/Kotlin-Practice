053d4898ebaec3409f6270ffbe18e246bdb3d2ca
Shoumik Panandikar <shoumikrp2000@gmail.com>
2022-05-31 17:45:16 +0530

    Added Images To Item

--- Layout/Affirmations/app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ Layout/Affirmations/app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,7 +1,12 @@
 package com.example.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 /**
  *   Forms the template for data (DTO)
  */
-data class Affirmation(val stringResourceId: Int){
-}
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
